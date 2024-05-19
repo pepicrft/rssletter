cd33063e7a33332a9265eeaefaff1b7e8938fd07
Pedro <pedro@pepicrft.me>
2024-05-19 16:30:04 +0200

    Configure fly deployment

---
+++ config/appsignal.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :appsignal, :config,
+  otp_app: :rssletter,
+  name: "rssletter",
+  push_api_key: "6704ace5-b6e5-48ab-91a3-5e37527919ad",
+  env: Mix.env
+
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\rssletter" eval Rssletter.Release.migrate
